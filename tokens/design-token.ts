4cf2160f1ace7913f5eeaa1983e9b5447ab1a265
Cass Woodhill <cwoodhill@kooth.com>
2021-10-01 15:10:41 +0100

    Export tokens/theme.js to tokens/theme.scss

--- tokens/design-token.ts
+++
@@ -1,6 +0,0 @@
-const colours = {
-  primary: "#00A797",
-  secondary: "#0F6F67",
-};
-
-export { colours };
---
+++ tokens/theme.js
@@ -0,0 +1,11 @@
+module.exports = {
+  colors: {
+    primary: "#FFFFFF",
+    accent: "#0099FF",
+  },
+  fontSize: {
+    small: ".875rem",
+    medium: "1rem",
+    large: "2rem",
+  },
+};
